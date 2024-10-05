e2d5623c27a309892a4c77b59e52687aecd2cd6d
leonardfelix <leonardfelix002@gmail.com>
2024-10-06 00:14:58 +1000

    add dish section

--- src/components/ProjectFirstPage.js
+++ src/components/ProjectFirstPage.js
@@ -1,11 +1,6 @@
-import { Container, Row, Col, Tab, Nav } from "react-bootstrap";
+import { Row, Tab, Nav } from "react-bootstrap";
 import { ProjectCard } from "./ProjectCard";
-import projImg1 from "../assets/img/project-img1.png";
-import projImg2 from "../assets/img/project-img2.png";
-import projImg3 from "../assets/img/project-img3.png";
-import colorSharp2 from "../assets/img/color-sharp2.png";
 
-import TrackVisibility from 'react-on-screen';
 
 // import figures for project
 
@@ -74,38 +69,6 @@ export const ProjectFirstPage = () => {
       },
     ];
   
-    const projects = [
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg2,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg1,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg2,
-      },
-      {
-        title: "Business Startup",
-        description: "Design & Development",
-        imgUrl: projImg3,
-      },
-    ];
   
     return (
       <div className="project-first">
